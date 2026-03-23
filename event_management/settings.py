8ff48d655da3bc96bdf145074fa591cfb46348ce
MD.Naim-UL- Haque <naimulh644@gmail.com>
2026-03-24 03:10:47 +0600

    Configure Cloudinary for media storage and Supabase for DB

--- event_management/settings.py
+++ event_management/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
     'events',
     "debug_toolbar",
     'users',
@@ -81,6 +84,14 @@ TEMPLATES = [
         },
     },
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET')
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 WSGI_APPLICATION = 'event_management.wsgi.application'
 
